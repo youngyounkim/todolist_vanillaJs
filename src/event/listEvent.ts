b070a106ddc94ee94cba4f7b77b9a5d297bfe26a
youngyounkim <rladuddbs789@gmail.com>
2023-12-04 16:40:28 +0900

    feat: list 제거 버튼 추가 및 이벤트 추가

--- src/event/listEvent.ts
+++ src/event/listEvent.ts
@@ -13,7 +13,19 @@ const listBox = getElementById<HTMLUListElement>('listBox');
 
 export const renderList: IrenderList = (listArr) => {};
 
+const deleteItem = (e: Event) => {
+  const element = e.target as Element;
+  const parent = element.parentNode;
+  listBox.removeChild(parent);
+};
+
 export const addItem: IaddItem = (itemName) => {
-  const item = setElement('li', `<p>${itemName}</p><button>제거</button>`);
+  const item = setElement('li', `<p>${itemName}</p>`);
+  const button = setElement('button');
+
+  button.addEventListener('click', deleteItem);
+  button.textContent = '삭제';
+
+  item.appendChild(button);
   listBox.appendChild(item);
 };
