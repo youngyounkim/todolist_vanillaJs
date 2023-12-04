3ffdd420b1f4c0fedc0ffd0ca53c2b3bb2c20aaa
youngyounkim <rladuddbs789@gmail.com>
2023-12-04 16:08:51 +0900

    feat: setElement 주석 추가 및 불필요한 콘텐츠 추가 방식 type 제거

--- src/utils/setElement.ts
+++ src/utils/setElement.ts
@@ -1,15 +1,18 @@
 interface IsetElement {
-  (
-    name: keyof HTMLElementTagNameMap,
-    content?: string,
-    InsertPosition?: 'afterbegin' | 'afterend' | 'beforebegin' | 'beforeend',
-  ): HTMLElementTagNameMap[keyof HTMLElementTagNameMap];
+  (name: keyof HTMLElementTagNameMap, content?: string): HTMLElementTagNameMap[keyof HTMLElementTagNameMap];
 }
 
-export const setElement: IsetElement = (name, content, InsertPosition = 'afterbegin') => {
+/**
+ *
+ * @param name {tagname} 생성할 html tag 이름
+ * @param content {string} 생성한 tag에 자식 콘텐츠로 추가될 정보
+ * @returns
+ */
+
+export const setElement: IsetElement = (name, content) => {
   const target = document.createElement(name);
   if (content) {
-    target.insertAdjacentHTML(InsertPosition, content);
+    target.insertAdjacentHTML('afterbegin', content);
   }
   return target;
 };
